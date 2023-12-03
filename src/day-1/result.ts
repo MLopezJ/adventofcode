3766086b637d5d530aa076b52a9e5bb7acf65dfd
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-03 15:36:16 +0100

    feat: result of day 1
    return sum of all of the calibration values

---
+++ src/day-1/result.ts
@@ -0,0 +1,26 @@
+import { readFile } from 'node:fs/promises'
+import path from 'node:path'
+import { getCalibration } from './getCalibration'
+
+/**
+ * Get tokens from calibrationDocument.txt
+ */
+const getTokens = () => {
+    const baseDir = process.cwd()
+    const subDir = (...tree: string[]): string => path.join(baseDir, ...tree)   
+    return readFile(subDir('src/day-1', 'calibrationDocument.txt'), 'utf-8').then((result => {
+        return result.toString().split('\n')
+    }));
+}
+
+/**
+ * return sum of all of the calibration values
+ */
+const Trebuchet = async () => {
+    const tokens =  await getTokens()
+    return tokens.map(token => getCalibration(token)).reduce((previous, current) => {
+        return previous + current
+    }, 0)
+}
+
+console.log(`the sum of all the calibration values is ${await Trebuchet()}`)
