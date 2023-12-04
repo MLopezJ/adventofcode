d59dd14a1019452ce898561f8f12dd4711cef106
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-04 11:09:58 +0100

    test: Get list of valid games and the sum of those ids
    failing test. WIP.

---
+++ src/day-2/getInfoFromGames.spec.ts
@@ -0,0 +1,60 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import { type Game } from './isGamePossible.js'
+
+void describe('getInfoFromGames', () => {
+	void it(`should return list of valid games and the sum of the IDs of those games`, () => {
+		const gamesList = [
+			{
+				id: 1,
+				rounds: [
+					{ red: 4, green: 0, blue: 3 },
+					{ red: 1, green: 2, blue: 6 },
+					{ red: 0, green: 2, blue: 0 },
+				],
+			},
+			{
+				id: 2,
+				rounds: [
+					{ red: 0, green: 2, blue: 1 },
+					{ red: 1, green: 3, blue: 4 },
+					{ red: 0, green: 1, blue: 1 },
+				],
+			},
+			{
+				id: 3,
+				rounds: [
+					{ red: 20, green: 8, blue: 6 },
+					{ red: 4, green: 13, blue: 5 },
+					{ red: 1, green: 5, blue: 0 },
+				],
+			},
+			{
+				id: 4,
+				rounds: [
+					{ red: 3, green: 1, blue: 6 },
+					{ red: 6, green: 3, blue: 0 },
+					{ red: 14, green: 3, blue: 15 },
+				],
+			},
+			{
+				id: 5,
+				rounds: [
+					{ red: 5, green: 3, blue: 1 },
+					{ red: 1, green: 2, blue: 2 },
+				],
+			},
+		]
+		const expected = getInfoFromGames(gamesList)
+		assert.equal(expected.validGames, [1, 2, 5])
+		assert.equal(expected.sumOfIds, 8)
+	})
+})
+
+/**
+ * Get list of valid games (ids) and the sum of those ids 
+ */
+const getInfoFromGames = (gamesList: Game[]) => ({
+	validGames: [1, 2, 50],
+	sumOfIds: 80,
+})
