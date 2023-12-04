1f29d499d2d5502ead0c84f782ba7951752ee919
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-04 17:00:57 +0100

    test: failing test for result of step 2
    WIP

---
+++ src/day-2/resultStep2.spec.ts
@@ -0,0 +1,63 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import type { Game } from './isGamePossible.js'
+
+void describe('resulStep2', () => {
+	void it(`should return the sum of the power given a set of games`, () => {
+		const gamesList = [
+			{
+				id: 1,
+				rounds: [
+					{ red: 4, green: 0, blue: 3 },
+					{ red: 1, green: 2, blue: 6 },
+					{ red: 0, green: 2, blue: 0 },
+				],
+			},
+			{
+				id: 2,
+				rounds: [
+					{ red: 0, green: 2, blue: 1 },
+					{ red: 1, green: 3, blue: 4 },
+					{ red: 0, green: 1, blue: 1 },
+				],
+			},
+			{
+				id: 3,
+				rounds: [
+					{ red: 20, green: 8, blue: 6 },
+					{ red: 4, green: 13, blue: 5 },
+					{ red: 1, green: 5, blue: 0 },
+				],
+			},
+			{
+				id: 4,
+				rounds: [
+					{ red: 3, green: 1, blue: 6 },
+					{ red: 6, green: 3, blue: 0 },
+					{ red: 14, green: 3, blue: 15 },
+				],
+			},
+			{
+				id: 5,
+				rounds: [
+					{ red: 5, green: 3, blue: 1 },
+					{ red: 1, green: 2, blue: 2 },
+				],
+			},
+		]
+
+		assert.equal(resulStep2(gamesList), 2286)
+	})
+})
+
+/**
+ * the sum of the power of games
+ */
+const resulStep2 = (games: Game[]) => {
+	return null
+}
+
+/**
+ * 
+ */
+const getPower = (sets: Game) => null
