4cf968e427996157479b630b2789337c549784a4
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-06 14:18:50 +0100

    fix: update method interface
    update param from getNumberAdjacentToSymbol

---
+++ src/day-3/getNumberAdjacentToSymbol.ts
@@ -0,0 +1,10 @@
+/**
+ * Adjacent contemplate up, down, left, rigth and diagonal
+ * dot (".") is not a symbol
+ */
+export const getNumberAdjacentToSymbol = (schematic: string[]): number[] => {
+    schematic.forEach(element => {
+        console.log(element)
+    });
+	return [0]
+}
