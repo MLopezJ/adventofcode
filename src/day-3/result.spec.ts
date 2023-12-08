83b49f0920501b967fab3616582e102d8faebdd4
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-08 12:10:30 +0100

    test: test gear ratios with 10 lines input
    failig test

--- src/day-3/result.spec.ts
+++ src/day-3/result.spec.ts
@@ -8,4 +8,34 @@ void describe('gearRatios', () => {
 		const number = gearRatios(logs)
 		assert.equal(number, 4361)
 	})
+
+	void it(`should sum all the part numbers of the engine schematic (10 lines)`, async () => {
+		const logs = await getEngineSchematic('engineSchematic-10.txt')
+		/**
+		 expected: 
+		    // 1
+			507,961, 668, 189, 906,
+			// 2 
+			805, 130, 880, 684, 17, 65, 91,
+			// 3
+			464, 208, 260, 967, 38, 692, 676, 247, 652, 74,
+			// 4
+			454, 859, 267, 
+			// 5
+			371, 484, 441, 224,
+			// 6
+			320, 883, 989, 733, 905, 946, 18, 515,
+			// 7 
+			880, 476, 955, 58, 23, 69, 102, 933, 687,
+			// 8
+			931, 757, 602, 983, 732, 
+			// 9
+			463, 183, 961, 572, 618, 52, 205, 429,
+			// 10
+			204, 550, 628, 240, 588, 167, 776
+		 */
+		const number = gearRatios(logs)
+
+		assert.equal(number, 33414)
+	})
 })
--- src/day-3/result.ts
+++ src/day-3/result.ts
@@ -38,4 +38,4 @@ const result = async () => {
 	)
 }
 
-result()
+//result()
