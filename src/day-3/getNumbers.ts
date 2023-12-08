60e9df9ba130e4d703f0017c01468eeb59c3171d
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-08 14:58:24 +0100

    fix: return number when it is at the end of the line

--- src/day-3/getNumbers.ts
+++ src/day-3/getNumbers.ts
@@ -50,33 +50,36 @@ const getList = (
 			{ list: undefined, carrier: undefined },
 		)
 
-
 const getNum = (init: number, end: number, tokens: string[]) => {
-    return [...Array(end - init + 1).keys()] // array with the positions
-				.map((i) => i + init)
-				.reduce((previous, current) => {
-                    //console.log(`${previous}${tokens[current]}`)
-					return `${previous}${tokens[current]}`
-				}, '')
+	return [...Array(end - init + 1).keys()] // array with the positions
+		.map((i) => i + init)
+		.reduce((previous, current) => {
+			//console.log(`${previous}${tokens[current]}`)
+			return `${previous}${tokens[current]}`
+		}, '')
 }
 /**
  * Get the numbers and position where it starts and end in the token
  */
 export const getNumbers = (tokens: string[]): NumberInfo[] => {
-	
 	const list = getList(tokens)
-	const x = list.list
+	const l =
+		list.carrier !== undefined && list.list !== undefined
+			? [...list.list, list.carrier]
+			: list.list
+
+	const x = l
 		?.filter((element) => element !== undefined)
-		.map( (element) => {
+		.map((element) => {
 			//let num = ''
-			
+
 			// return num
-            const num =  getNum(element.init, element.end, tokens)
-            return {...element, number: Number(num)}
+			const num = getNum(element.init, element.end, tokens)
+			return { ...element, number: Number(num) }
 		})
 	//console.log()
-    return x as NumberInfo[]
-    /*
+	return x as NumberInfo[]
+	/*
 	return [
 		{ number: 467, init: 0, end: 2 },
 		{ number: 114, init: 5, end: 7 },
