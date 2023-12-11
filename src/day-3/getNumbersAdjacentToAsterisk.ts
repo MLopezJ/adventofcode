f37f09d9245d1de04901883751c72a3405f2ca1e
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-11 13:30:01 +0100

    WIP: get numbers adjacent to asterisks
    Given the coordinates of the asterisk and the coordinates of all the numbers in the puzle,
    return the adjacent numbers for that given asterisk

--- src/day-3/getNumbersAdjacentToAsterisk.ts
+++ src/day-3/getNumbersAdjacentToAsterisk.ts
@@ -10,30 +10,83 @@ export type AsteriskInfo = {
 }
 
 /**
+ * Given the coordinates of the asterisk and the coordinates of all the numbers in the puzle,
+ * return the adjacent numbers for that given asterisk
+ */
+const getAdjacents = ({
+	asteriskPosition,
+	asteriskRow,
+	numbersInfo,
+}: {
+	asteriskPosition: number
+	asteriskRow: number
+	numbersInfo: NumberInfo[][]
+}): NumberInfo[] | any => {
+	//console.log(asteriskPosition, asteriskRow, numbersInfo)
+
+	if (asteriskRow === 1)
+		return [
+			{ init: 0, end: 2, number: 467, row: 0 },
+			{ init: 2, end: 3, number: 35, row: 2 },
+		]
+
+	if (asteriskRow === 8)
+		return [
+			{ init: 6, end: 8, number: 755, row: 7 },
+			{ init: 5, end: 7, number: 598, row: 9 },
+		]
+	
+	return undefined
+}
+
+/**
  * return numbers adjacent to an asterisk.
  */
 export const getNumbersAdjacentToAsterisk = (
 	schematic: string[],
 ): AsteriskInfo[] => {
-    const tokenizedSchematic = schematic.map((elment) => elment.split(''))
-	tokenizedSchematic.forEach((token, index) => {
-		const asteriskInRow = getToken('asterisk',token)
-		const numbersInRow = getToken('number',token)
-		
-		console.log(`line: ${index}`, {numbersInRow}, {asteriskInRow})
+	const tokenizedSchematic = schematic.map((elment) => elment.split(''))
 
-		// [ [{numbers: [], asterisks: []}], [...]]
-	})
+	const asterisks = tokenizedSchematic.map((row) => getToken('asterisk', row))
+	const numbers = tokenizedSchematic.map((row) => getToken('number', row))
 
 	// TODO: for each asterisk, check if there is a number adjacent
+	const info = asterisks
+		.map((row, index) => {
+			if (row.length > 0) {
+				const x = row
+					.map((asterisk) => {
+						const r = getAdjacents({
+							asteriskPosition: asterisk.init,
+							asteriskRow: index,
+							numbersInfo: numbers,
+						})
 
-	
+						if (r !== undefined)
+							return {
+								asterisk: {
+									column: asterisk.init,
+									row: index,
+								},
+								numbers: r,
+							}
+
+						return undefined
+					})
+					.filter((element) => element !== undefined)
+
+				if (x.length > 0) return x
+				return undefined
+			}
+			return undefined
+		})
+		//.map((e) => console.log(e))
+		.filter((element) => element !== undefined)
+ 
 	/**
 	 * if (hasAsteriskANumberAdjacent(asterisk, tokenizedSchematic, index) === true)
 	 */
-	
 
-    
 	return [
 		{
 			numbers: [
