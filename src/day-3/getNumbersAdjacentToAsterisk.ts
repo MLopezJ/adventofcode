c08abfc783fad4404b2322ced03034482f2fdbe1
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-12 15:19:01 +0100

    feat: returrn adjacents numbers to an asterisk
    Given the coordinates of the asterisk and the coordinates of all the numbers in the puzle,
    return the adjacent numbers for that given asterisk

--- src/day-3/getNumbersAdjacentToAsterisk.ts
+++ src/day-3/getNumbersAdjacentToAsterisk.ts
@@ -22,105 +22,155 @@ export const getAdjacents = ({
 	asteriskRow: number
 	numbersInfo: NumberInfo[][]
 }): NumberInfo[] | any => {
-	numbersInfo.map((row, rowIndex) => {
-		if (row.length > 0) {
-			row.map((info) => {
-				/**
-				 * because of adjacent definition, make no sense to check more than
-				 * the row up and down to the asterisk
-				 */
+	const result = numbersInfo
+		.map((row, rowIndex) => {
+			if (row.length > 0) {
+				const adjacentsInRow = row
+					.map((info) => {
+						/**
+						 * because of adjacent definition, make no sense to check more than
+						 * the row up and down to the asterisk
+						 */
 
-				// same line
-				if (rowIndex === asteriskRow) {
-					// an asterisk is in the right to this position
-					if (asteriskPosition > 0 && info.end + 1 === asteriskPosition) {
-						console.log(
-							'number ',
-							info.number,
-							' is same row. In the right there is an asterisk',
-						)
-					}
+						// same line
+						if (rowIndex === asteriskRow) {
+							// an asterisk is in the right to this position
+							if (asteriskPosition > 0 && info.end + 1 === asteriskPosition) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									' is same row. In the right there is an asterisk',
+								)*/
+								return { ...info, row: rowIndex }
+							}
 
-					// an asterisk is in the left to this position
-					if (asteriskPosition < 9 && info.init - 1 === asteriskPosition) {
-						console.log(
-							'number ',
-							info.number,
-							' is same row. In the left there is an asterisk',
-						)
-					}
-				}
+							// an asterisk is in the left to this position
+							if (asteriskPosition < 9 && info.init - 1 === asteriskPosition) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									' is same row. In the left there is an asterisk',
+								)*/
+								return { ...info, row: rowIndex }
+							}
+						}
 
-				// taking the number as reference, the asterisk is 1 line down
-				if (asteriskRow > rowIndex && asteriskRow - rowIndex === 1) {
-					// an asterisk is down to this position
-					if (info.init <= asteriskPosition && info.end >= asteriskPosition) {
-						console.log('number ', info.number, '.  Down there is an asterisk')
-					}
+						// taking the number as reference, the asterisk is 1 line down
+						if (asteriskRow > rowIndex && asteriskRow - rowIndex === 1) {
+							// an asterisk is down to this position
+							if (
+								info.init <= asteriskPosition &&
+								info.end >= asteriskPosition
+							) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									'.  Down there is an asterisk',
+								)
+								*/
+								return { ...info, row: rowIndex }
+							}
 
-					// an asterisk is diagonal rigth to this position
-					if (asteriskPosition > 0 && info.end + 1 === asteriskPosition) {
-						console.log(
-							'number ',
-							info.number,
-							'. an asterisk is diagonal rigth (down) to this position',
-						)
-					}
+							// an asterisk is diagonal rigth to this position
+							if (asteriskPosition > 0 && info.end + 1 === asteriskPosition) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									'. an asterisk is diagonal rigth (down) to this position',
+								)*/
+								return { ...info, row: rowIndex }
+							}
 
-					// an asterisk is diagonal left to this position
-					if (asteriskPosition < 9 && info.init - 1 === asteriskPosition) {
-						console.log(
-							'number ',
-							info.number,
-							' is diagonal left (down) to this position',
-						)
-					}
-				}
+							// an asterisk is diagonal left to this position
+							if (asteriskPosition < 9 && info.init - 1 === asteriskPosition) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									' is diagonal left (down) to this position',
+								)
+								*/
+								return { ...info, row: rowIndex }
+							}
+						}
 
-				// taking the number as reference, the asterisk is 1 line up
-				if (rowIndex > asteriskRow && rowIndex - asteriskRow === 1) {
-					// an asterisk is up to this position
-					if (info.init <= asteriskPosition && info.end >= asteriskPosition) {
-						console.log('number ', info.number, '.  Up there is an asterisk')
-					}
+						// taking the number as reference, the asterisk is 1 line up
+						if (rowIndex > asteriskRow && rowIndex - asteriskRow === 1) {
+							// an asterisk is up to this position
+							if (
+								info.init <= asteriskPosition &&
+								info.end >= asteriskPosition
+							) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									'.  Up there is an asterisk',
+								)
+								*/
+								return { ...info, row: rowIndex }
+							}
 
-					// an asterisk is diagonal rigth to this position
-					if (asteriskPosition > 0 && info.end + 1 === asteriskPosition) {
-						console.log(
-							'number ',
-							info.number,
-							'. an asterisk is diagonal rigth (up) to this position',
-						)
-					}
+							// an asterisk is diagonal rigth to this position
+							if (asteriskPosition > 0 && info.end + 1 === asteriskPosition) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									'. an asterisk is diagonal rigth (up) to this position',
+								)
+								*/
+								return { ...info, row: rowIndex }
+							}
 
-					// an asterisk is diagonal left to this position
-					if (asteriskPosition < 9 && info.init - 1 === asteriskPosition) {
-						console.log(
-							'number ',
-							info.number,
-							' is diagonal left (down) to this position',
-						)
-					}
-					
-				}
-			})
+							// an asterisk is diagonal left to this position
+							if (asteriskPosition < 9 && info.init - 1 === asteriskPosition) {
+								/*
+								console.log(
+									'number ',
+									info.number,
+									' is diagonal left (down) to this position',
+								)
+								*/
+								return { ...info, row: rowIndex }
+							}
+						}
+
+						return undefined
+					})
+					.filter((element) => element !== undefined)
+
+				if (adjacentsInRow.length === 0) return undefined
+				return adjacentsInRow
+			}
+			return undefined
+		})
+		.filter((element) => element !== undefined)
+
+	//console.log(result)
+
+	// remove matrix format from result
+	return result.reduce((previus, current) => {
+		if (previus === undefined) {
+			return [
+				current?.reduce((p, c) => {
+					return { ...p, ...c }
+				}),
+			]
 		}
-	})
 
-
-	if (asteriskRow === 1)
-		return [
-			{ init: 0, end: 2, number: 467, row: 0 },
-			{ init: 2, end: 3, number: 35, row: 2 },
-		]
-
-	if (asteriskRow === 8)
-		return [
-			{ init: 6, end: 8, number: 755, row: 7 },
-			{ init: 5, end: 7, number: 598, row: 9 },
-		]
-
-	return undefined
+		const temp = previus
+		temp.push(
+			current?.reduce((p, c) => {
+				return { ...p, ...c }
+			}),
+		)
+		return temp
+	}, undefined)
 }
 
 /**
