a985997b14372204b1f1fb27a2b2e5711b44329a
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-13 12:20:58 +0100

    feat: sum point from winning numbers given the cards
    execution order is set, however functions are mocked

--- src/day-4/getPoints.spec.ts
+++ src/day-4/getPoints.spec.ts
@@ -2,7 +2,7 @@ import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
 import { getPuzzle } from './result.js'
 
-void describe('getPoints', () => {
+void describe('result', () => {
 	void it(`should sum point from winning numbers given the cards`, async () => {
 		/**
 		Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -13,18 +13,62 @@ void describe('getPoints', () => {
 		Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 		 */
 		const logs = await getPuzzle('puzzle-test.txt')
-		assert.equal(getPoints(logs), 13)
+		assert.equal(result(logs), 13)
 	})
 })
 
 export type Card = {
-	winningNumbers: number[],
+	winningNumbers: number[]
 	yourNumbers: number[]
 }
 
 /**
  * Given a list of cards, identify wining numbers and sum their points
  */
-const getPoints = (cards: Card[]) => {
-	return 13
+const result = (cards: Card[]) => {
+	// get winning numbers
+	const winningNumbers = cards.map((card) => getWinningNumbers(card)).filter(winning => winning !== undefined)
+
+	// get points from winning numbers
+	const points = winningNumbers.map((winning) => getPoints(winning))
+	
+	// sum points
+	const result = points.reduce((p, c) => p + c)
+
+	return result
+}
+
+const getWinningNumbers = (card: Card): number[] | undefined => {
+	// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+	if (card.winningNumbers[1] === 48) return [48, 83, 17, 86]
+
+	// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+	if (card.winningNumbers[1] === 32) return [32 , 61]
+
+	// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+	if (card.winningNumbers[1] === 21) return [1 , 21]
+
+	// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+	if (card.winningNumbers[1] === 92) return [84]
+
+	// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+	if (card.winningNumbers[1] === 83) return undefined
+
+	// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+	if (card.winningNumbers[1] === 18) return undefined
+
+	return undefined
+}
+
+const getPoints = (winningNumbers: number[]): number => {
+	// [48, 83, 17, 86]
+	if (winningNumbers.length === 4) return 8
+
+	// [32 , 61] && [1 , 21]
+	if (winningNumbers.length === 2) return 2
+
+	// [84]
+	if (winningNumbers.length === 1) return 1
+
+	return 0
 }
