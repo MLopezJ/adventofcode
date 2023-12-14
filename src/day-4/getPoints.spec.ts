7004aae569f05f2bf777294e334052d105e1f550
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-14 13:36:00 +0100

    test: sum points from winning numbers of cards
    build test, mock implementation +

--- src/day-4/getPoints.spec.ts
+++ src/day-4/getPoints.spec.ts
@@ -1,6 +1,46 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
-import { getPuzzle } from './result.js'
+import { getPoints } from './getPoints'
 
-
-
+void describe('getPoints', () => {
+	for (const [input, expected] of [
+		[
+			{
+				id: 1,
+				matches: [48, 83, 17, 86],
+			},
+			8,
+		],
+		[
+			{
+				id: 2,
+				matches: [32 , 61],
+			},
+			2,
+		],
+		[
+			{
+				id: 3,
+				matches: [1 , 21],
+			},
+			2,
+		],
+		[
+			{
+				id: 4,
+				matches: [84],
+			},
+			1,
+		],
+	] as [
+		{
+			id: number
+			matches: number[]
+		},
+		number,
+	][]) {
+		void it(`should sum points from winning numbers. Card`, () => {
+			assert.equal(getPoints(input.matches), expected)
+		})
+	}
+})
--- src/day-4/getPoints.ts
+++ src/day-4/getPoints.ts
@@ -1,3 +1,9 @@
+
+/**
+ * Each element from input list is considerr as a match.
+ * The first match makes the card worth one point.
+ * Each match after the first doubles the point value of that card.
+ */
 export const getPoints = (winningNumbers: number[]): number => {
 	// [48, 83, 17, 86]
 	if (winningNumbers.length === 4) return 8
