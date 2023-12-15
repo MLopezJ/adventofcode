e172ee160341f021675db5e135ccf2115920190b
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-15 16:21:04 +0100

    feat: never make a copy if a card past the end of the table

--- src/day-4/processCards.spec.ts
+++ src/day-4/processCards.spec.ts
@@ -116,4 +116,81 @@ void describe('processCards', () => {
 
 		assert.deepEqual(processCards(input), expected)
 	})
+
+    void it(`should never make a copy if a card past the end of the table.`, () => {
+		const input = {
+			1: {
+				numbers: {
+					winningNumbers: [41, 48, 83, 86, 17],
+					yourNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+				},
+				matches: 0,
+				copies: 0,
+			},
+
+			2: {
+				numbers: {
+					winningNumbers: [13, 32, 20, 16, 61],
+					yourNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
+				},
+				matches: 0,
+				copies: 0,
+			},
+
+			3: {
+				numbers: {
+					winningNumbers: [1, 21, 53, 59, 44],
+					yourNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+				},
+				matches: 0,
+				copies: 0,
+			},
+
+			4: {
+				numbers: {
+					winningNumbers: [41, 92, 73, 84, 69],
+					yourNumbers: [59, 84, 76, 51, 58, 5, 54, 83],
+				},
+				matches: 0,
+				copies: 0,
+			},
+		}
+
+		const expected = {
+			'1': {
+				matches: 4,
+				copies: 0,
+                numbers: {
+					winningNumbers: [41, 48, 83, 86, 17],
+					yourNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+				}
+			},
+			'2': {
+				matches: 2,
+				copies: 1,
+                numbers: {
+					winningNumbers: [13, 32, 20, 16, 61],
+					yourNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
+				},
+			},
+			'3': {
+				matches: 2,
+				copies: 3,
+                numbers: {
+					winningNumbers: [1, 21, 53, 59, 44],
+					yourNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+				},
+			},
+			'4': {
+				matches: 1,
+				copies: 7,
+                numbers: {
+					winningNumbers: [41, 92, 73, 84, 69],
+					yourNumbers: [59, 84, 76, 51, 58, 5, 54, 83],
+				},
+			},
+		}
+
+		assert.deepEqual(processCards(input), expected)
+	})
 })
--- src/day-4/processCards.ts
+++ src/day-4/processCards.ts
@@ -6,49 +6,52 @@ const numberRange = (start: number, end: number) =>
 
 /**
  * Discover the amount of matches and copies per card.
- * 
+ *
  * A match is when a number of "winning numbers" is in "your numbers".
- * 
+ *
  * As a result of a match, you will win a copy of a card.
  * The amount of matches is taken in cosideration to target which cards are going to be copied.
  * So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
- * 
+ *
  * A copy behave exactly equal than the original instance, for that reason, the rule of "find the match and add a copy to
- * others cards" applies to them as well. 
+ * others cards" applies to them as well.
  *
  * (Cards will never make you copy a card past the end of the table.)
  */
 export const processCards = (cards: Scratchcards): Scratchcards => {
-	Object.entries(cards).map((element) => {
+	const table = Object.entries(cards)
+	const limitOfTable = table.length
+	table.map((element) => {
 		const [id, card] = element
 		const cardId = Number(id)
 
-        /**
-         * Get number of matches.
-         * As a result of a match, you will win a copy of a card.
-         */
+		/**
+		 * Get number of matches.
+		 * As a result of a match, you will win a copy of a card.
+		 */
 		const amountOfMatches = getWinningNumbers(card.numbers).length
 		;(cards[cardId] as Scratchcard).matches = amountOfMatches
 
-        /**
-         * The amount of matches is taken in cosideration to target which cards are going to be copied.
-         */
+		/**
+		 * The amount of matches is taken in cosideration to target which cards are going to be copied.
+		 */
 		const init = cardId + 1
 		const end = amountOfMatches + init
 		const targetCards = numberRange(init, end)
 		if (targetCards.length > 0) {
 			targetCards.map((indexOfCard) => {
+				if (indexOfCard <= limitOfTable) {
+					const currentCard = cards[cardId] as Scratchcard
+					/**
+					 * the number of copies already won by that card + the original
+					 */
+					const copies = currentCard.copies + 1
 
-                const currentCard = (cards[cardId] as Scratchcard)
-                /**
-                 * the number of copies already won by that card + the original
-                 */
-				const copies = currentCard.copies + 1
-
-                /**
-                 * Sum copies to targered card
-                 */
-				;(cards[indexOfCard] as Scratchcard).copies += copies
+					/**
+					 * Sum copies to targered card
+					 */
+					;(cards[indexOfCard] as Scratchcard).copies += copies
+				}
 			})
 		}
 	})
