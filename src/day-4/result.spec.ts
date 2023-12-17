838ef85481b1ac233c59b4d1d7896b3defc5608a
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-17 13:24:04 +0100

    fix: use method
    remove mock imput and use method

--- src/day-4/result.spec.ts
+++ src/day-4/result.spec.ts
@@ -4,6 +4,7 @@ import {
 	getInput,
 	partIFormat,
 	partII,
+	partIIFormat,
 	result,
 	transformToGameType,
 	transformToGameTypePartII,
@@ -66,66 +67,7 @@ void describe('partII', () => {
 		Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 		Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 		 */
-		//const logs = await getPuzzle('puzzle-test.txt')
-
-		// TODO: transform input in expected type
-		const logs = {
-			1: {
-				numbers: {
-					winningNumbers: [41, 48, 83, 86, 17],
-					yourNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
-				},
-				matches: 0,
-				copies: 0,
-			},
-
-			2: {
-				numbers: {
-					winningNumbers: [13, 32, 20, 16, 61],
-					yourNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
-				},
-				matches: 0,
-				copies: 0,
-			},
-
-			3: {
-				numbers: {
-					winningNumbers: [1, 21, 53, 59, 44],
-					yourNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
-				},
-				matches: 0,
-				copies: 0,
-			},
-
-			4: {
-				numbers: {
-					winningNumbers: [41, 92, 73, 84, 69],
-					yourNumbers: [59, 84, 76, 51, 58, 5, 54, 83],
-				},
-				matches: 0,
-				copies: 0,
-			},
-
-			5: {
-				numbers: {
-					winningNumbers: [87, 83, 26, 28, 32],
-					yourNumbers: [88, 30, 70, 12, 93, 22, 82, 36],
-				},
-
-				matches: 0,
-				copies: 0,
-			},
-
-			6: {
-				numbers: {
-					winningNumbers: [31, 18, 13, 56, 72],
-					yourNumbers: [74, 77, 10, 23, 35, 67, 36, 11],
-				},
-
-				matches: 0,
-				copies: 0,
-			},
-		}
+		const logs = await getInput('puzzle-test.txt', partIIFormat)
 		assert.equal(partII(logs), 30)
 	})
 })
