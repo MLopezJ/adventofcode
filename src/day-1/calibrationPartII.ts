d12f52a6c5ea0d3ca2fc7695d258271a23fcf636
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-19 18:48:07 +0100

    WIP: get calibration number
    receive, identify the first and last number in the string and return the number as two digit number
    
    the number to indetify can be in string format (five) or in number format (5)

---
+++ src/day-1/calibrationPartII.ts
@@ -0,0 +1,24 @@
+/**
+ * From string to number
+ */
+const numbers = {
+	one: 1,
+	two: 2,
+	three: 3,
+	four: 4,
+	five: 5,
+	six: 6,
+	seven: 7,
+	eight: 8,
+	nine: 9,
+}
+
+/**
+ * Find first and last number of input.
+ *
+ * Number can be writen in numberr format (example: 5) or in string format (example: five)
+ */
+export const calibrationPartII = (token: string): number => {
+	console.log([...token])
+	return 0
+}
