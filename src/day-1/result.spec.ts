66832018ce9f258de86090577ae45cf1e5dcdfb7
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-21 18:29:49 +0100

    fix: sum result of calibration
    Create generic method to sum the result of calibrate the token.
    
    Method is used to generate result of step 1 and step 2 of day 1

--- src/day-1/result.spec.ts
+++ src/day-1/result.spec.ts
@@ -1,12 +1,11 @@
 import { describe, it } from "node:test"
 import assert from "node:assert/strict"
-import { TrebuchetII, getTokens } from "./result.js"
+import { getTokens, sum } from "./result.js"
+import { calibrationPartII } from "./calibrationPartII.js"
 
 void describe(`part II`, () => {
-    it(`should generate result for step II of day 1`, async () => {
-        const tokens =  await getTokens('example.txt')
-        const result = TrebuchetII(tokens)
-        /**
+    it(`should sum the numbers received from token after apply the calibration of part II`, async () => {
+         /**
          * 
             4dtlmkfnm  --> 44
             four539tkqrc  -- > 49
@@ -16,6 +15,8 @@ void describe(`part II`, () => {
             kncvqpzdtfs7 --> 77
             6seveneighttwonine2 --> 62
          */
-        assert.equal(result, 360) 
+        const tokens =  await getTokens('example.txt')
+        const partII = sum(tokens,calibrationPartII )
+        assert.equal(partII, 360) 
     })
 })
--- src/day-1/result.ts
+++ src/day-1/result.ts
@@ -9,36 +9,36 @@ import { calibrationPartII } from './calibrationPartII.js'
 export const getTokens = async (file: string) => {
 	const baseDir = process.cwd()
 	const subDir = (...tree: string[]): string => path.join(baseDir, ...tree)
-	return readFile(subDir('src/day-1', file), 'utf-8').then(
-		(result) => {
-			return result.toString().split('\n')
-		},
-	)
+	return readFile(subDir('src/day-1', file), 'utf-8').then((result) => {
+		return result.toString().split('\n')
+	})
 }
 
 /**
  * return sum of all of the calibration values
  */
-const Trebuchet = async () => {
+export const sum = (
+	tokens: string[],
+	calibration: (token: string) => number,
+) => {
+	return tokens
+		.map((token) => calibration(token))
+		.reduce((previous, current) => {
+			return previous + current
+		}, 0)
+}
+
+/**
+ * result of part I and part II of day I
+ */
+const result = async () => {
 	const tokens = await getTokens('calibrationDocument.txt')
-	return tokens
-		.map((token) => getCalibration(token))
-		.reduce((previous, current) => {
-			return previous + current
-		}, 0)
+
+	const partI = sum(tokens, getCalibration)
+	const partII = sum(tokens, calibrationPartII)
+
+	console.log(`the sum of all the calibration values (part I) is ${partI}`)
+	console.log(`the sum of all the calibration values (part II) is ${partII}`)
 }
 
-/**
- * return sum of all of the calibration values
- */
-export const TrebuchetII = (tokens: string[]) => {
-	return tokens
-		.map((token) => calibrationPartII(token))
-		.reduce((previous, current) => {
-			return previous + current
-		}, 0)
-}
-
-console.log(`the sum of all the calibration values is ${await Trebuchet()}`)
-const tokensPartII = await getTokens('calibrationDocument.txt')
-console.log(`the sum of all the calibration values (part II) is ${TrebuchetII(tokensPartII)}`)
+result()
