3b2e667f39a4f4902e56bef39a541288f8e20873
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-23 18:07:24 +0100

    WIP: navigate map

--- src/day-8/navigate.ts
+++ src/day-8/navigate.ts
@@ -1,36 +1,47 @@
-
 type NeightbornNode = {
-    left: string,
-    rigth: string
+	left: string
+	rigth: string
 }
 
-type Map = Record<string,NeightbornNode>
+type Map = Record<string, NeightbornNode>
 
 /**
- * Follow the instructions to archive arriving goal from departure node 
+ * Follow the instructions to archive arriving goal from departure node
  * and return the amount of steps it takes
  */
 export const navigate = ({
-    departure,
-    arrive,
-    instructions,
-    map
+	departure,
+	arrive,
+	instructions,
+	map,
 }: {
-    departure: string,
-    arrive: string,
-    instructions: string[],
-    map: Map
+	departure: string
+	arrive: string
+	instructions: string[]
+	map: Map
 }): number => {
+	let currentNode = map[departure]
+    console.log('init, ',departure, currentNode)
+	// max amount of steps possible. (used to prevent infinite loops)
+	let steps = 10
+	let counter = 0
 
-    let currentNode = arrive
-    // max amount of steps possible. (used to prevent infinite loops)
-    const steps = 10
+	/* currentNode !== arrive || */
+	while (steps > 0) {
+		//console.log(map[currentNode])
+		const instruction = instructions[counter]
+		console.log(`move to ${instruction}`)
+		if (instruction === 'L') {
+            console.log(currentNode?.left)
+            currentNode = map[currentNode?.left]
+        }
+		else {
+			console.log(currentNode?.rigth)
+            currentNode = map[currentNode?.rigth]
+		}
+		steps -= 1
+		counter += 1
+	}
 
-    /*
-    while(currentNode !== arrive || steps > 0){
-
-    }
-    */
-
-    return 2
+	return 2
 }
