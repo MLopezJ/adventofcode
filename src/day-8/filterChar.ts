d0224bfa24698d592cd01b0a7afd33c3403553e7
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-25 21:24:55 +0100

    feat: filter char
    Given an string and a char, remove the char from string

--- src/day-8/filterChar.ts
+++ src/day-8/filterChar.ts
@@ -1,7 +1,15 @@
 /**
  * Given an string and a char, remove the char from string
- * 
+ *
  */
-export const filterChar =  (txt: string, char: string) => {
-    return txt
+export const filterChar = (txt: string, char: string) => {
+	const result = txt
+		.split(char)
+		.filter((element) => element !== char)
+		.reduce((previous, current) => {
+            // concat string
+			return `${previous}${current}`
+		})
+	console.log(result)
+	return result
 }
