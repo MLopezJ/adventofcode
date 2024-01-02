6eabddc2b45c617a19f245dcdd3e931271be2e08
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-01-02 18:34:08 +0100

    test: navigate simultaneous
    navigate 2 paths at the same time
    
    mocking test

---
+++ src/day-8/navigateSimultaneous.spec.ts
@@ -0,0 +1,42 @@
+// should reach ZZ following instructions
+// steps 2
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+
+void describe(`navigateSimultaneous`, () => {
+	void it(`should navigate 2 paths at the same time`, async () => {
+		const path = 'logs-testIII.txt'
+		/**
+            LR
+
+            11A = (11B, XXX)
+            11B = (XXX, 11Z)
+            11Z = (11B, XXX)
+            22A = (22B, XXX)
+            22B = (22C, 22C)
+            22C = (22Z, 22Z)
+            22Z = (22B, 22B)
+            XXX = (XXX, XXX)
+         */
+		const departure = 'A'
+		const arrive = 'Z'
+		const steps = await navigateSimultaneous({
+			departure,
+			arrive,
+			path,
+		})
+		assert.equal(steps, 6)
+	})
+})
+
+const navigateSimultaneous = async ({
+	departure,
+	arrive,
+	path,
+}: {
+	departure: string
+	arrive: string
+	path: string
+}) => {
+    return 6
+}
