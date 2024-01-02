e42ca50cb73f1efd619fd9ab0b2ec9aa14f4b0c4
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-01-02 17:08:20 +0100

    fix: update navigate API
    receive file path as parameter

--- src/day-8/navigate.spec.ts
+++ src/day-8/navigate.spec.ts
@@ -5,7 +5,8 @@ import assert from 'node:assert/strict'
 import { navigate } from './navigate.js'
 
 void describe(`navigate`, () => {
-	it(`should reach ZZZ from AAA following instructions`, () => {
+	it(`should reach ZZZ from AAA following instructions`, async () => {
+		const path = 'logs-test.txt'
 		/**
             RL
 
@@ -16,61 +17,13 @@ void describe(`navigate`, () => {
             EEE = (EEE, EEE)
             GGG = (GGG, GGG)
             ZZZ = (ZZZ, ZZZ)
-
          */
-
-		const map = {
-			// AAA = (BBB, CCC)
-			AAA: {
-				left: 'BBB',
-				rigth: 'CCC',
-			},
-
-			// BBB = (DDD, EEE)
-			BBB: {
-				left: 'DDD',
-				rigth: 'EEE',
-			},
-
-			// CCC = (ZZZ, GGG)
-			CCC: {
-				left: 'ZZZ',
-				rigth: 'GGG',
-			},
-
-			// DDD = (DDD, DDD)
-			DDD: {
-				left: 'DDD',
-				rigth: 'DDD',
-			},
-
-			// EEE = (EEE, EEE)
-			EEE: {
-				left: 'EEE',
-				rigth: 'EEE',
-			},
-
-			// GGG = (GGG, GGG)
-			GGG: {
-				left: 'GGG',
-				rigth: 'GGG',
-			},
-
-			// ZZZ = (ZZZ, ZZZ)
-			ZZZ: {
-				left: 'ZZZ',
-				rigth: 'ZZZ',
-			},
-		}
-
 		const departure = 'AAA'
 		const arrive = 'ZZZ'
-		const instructions = ['R', 'L']
-		const steps = navigate({
+		const steps = await navigate({
 			departure,
 			arrive,
-			instructions,
-			map,
+			path,
 		})
 		assert.equal(steps, 2)
 	})
--- src/day-8/navigate.ts
+++ src/day-8/navigate.ts
@@ -1,3 +1,5 @@
+import { readFile } from './readFile.js'
+
 type NeightbornNode = {
 	left: string
 	rigth: string
@@ -9,35 +11,33 @@ export type Map = Record<string, NeightbornNode>
  * Follow the instructions to archive arriving goal from departure node
  * and return the amount of steps it takes
  */
-export const navigate = ({
+export const navigate = async ({
 	departure,
 	arrive,
-	instructions,
-	map,
+	path,
 }: {
 	departure: string
 	arrive: string
-	instructions: string[]
-	map: Map
-}): number => {
-    let currentNode = departure
+	path: string
+}): Promise<number> => {
+	const data = await readFile(path)
+	let currentNode = departure
 
 	// max amount of steps possible. (used to prevent infinite loops)
 	let steps = 10
-    // iterate over the instructions
+	// iterate over the instructions
 	let iterator = 0
 
 	/* currentNode !== arrive || */
 	while (currentNode !== arrive || steps > 0) {
-		const instruction = instructions[iterator]
-        steps -= 1
+		const instruction = data.instructions[iterator]
+		steps -= 1
 		iterator += 1
-        
-		if (instruction === 'L')currentNode = map[currentNode]?.left as string
-		else currentNode = map[currentNode]?.rigth  as string
 
-        if (currentNode === arrive) break
-		
+		if (instruction === 'L') currentNode = data.map[currentNode]?.left as string
+		else currentNode = data.map[currentNode]?.rigth as string
+
+		if (currentNode === arrive) break
 	}
 
 	return iterator
