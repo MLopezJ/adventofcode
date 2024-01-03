d12d22733869724828cfe7cb6b8ea9b46eecf267
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-01-03 05:03:10 +0100

    fix: create new file

--- src/day-8/navigateSimultaneous.spec.ts
+++ src/day-8/navigateSimultaneous.spec.ts
@@ -1,8 +1,6 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
-import { readFile } from './readFile.js'
-import { inspectNode } from './inspectNode.js'
-import type { Map } from './navigate.js'
+import { navigateSimultaneous } from './navigateSimultaneous.js'
 
 void describe(`navigateSimultaneous`, () => {
 	void it(`should navigate 2 paths at the same time`, async () => {
@@ -30,54 +28,3 @@ void describe(`navigateSimultaneous`, () => {
 	})
 })
 
-/**
- * Navigate multiple paths simultaneous to archive final node
- */
-const navigateSimultaneous = async ({
-	departure,
-	arrive,
-	path,
-}: {
-	departure: string
-	arrive: string
-	path: string
-}) => {
-	const data = await readFile(path)
-
-	// iterate over the instructions
-	let iterator = 0
-	let array = Object.keys(data.map).filter((node) =>
-		inspectNode(node, departure),
-	)
-
-    // TODO: add descrption
-    let check = array.every((element) => inspectNode(element, arrive))
-
-	while(check !== true) {
-		const index = iterator % data.instructions.length
-		const instruction = data.instructions[index] as string
-        
-		array = array.map((element) => step(element, instruction, data.map))
-		
-        check = array.every((element) => inspectNode(element, arrive))
-		iterator += 1
-	}
-   
-	return iterator
-}
-
-/**
- * Move to node following instruction
- */
-const step = (currentNode: string, instruction: string, map: Map) => {
-	const direction = getDirection(instruction)
-	return (map[currentNode] as any)[direction]
-}
-
-/**
- * Translate instruction to direction
- */
-const getDirection = (direrction: string) => {
-	if (direrction === 'L') return 'left'
-	return 'rigth'
-}
---
+++ src/day-8/navigateSimultaneous.ts
@@ -0,0 +1,55 @@
+import { readFile } from './readFile.js'
+import { inspectNode } from './inspectNode.js'
+import type { Map } from './navigate.js'
+
+/**
+ * Navigate multiple paths simultaneous to archive final node
+ */
+export const navigateSimultaneous = async ({
+	departure,
+	arrive,
+	path,
+}: {
+	departure: string
+	arrive: string
+	path: string
+}) => {
+	const data = await readFile(path)
+
+	// iterate over the instructions
+	let iterator = 0
+	let array = Object.keys(data.map).filter((node) =>
+		inspectNode(node, departure),
+	)
+
+    // TODO: add descrption
+    let check = array.every((element) => inspectNode(element, arrive))
+
+	while(check !== true) {
+		const index = iterator % data.instructions.length
+		const instruction = data.instructions[index] as string
+        
+		array = array.map((element) => step(element, instruction, data.map))
+		
+        check = array.every((element) => inspectNode(element, arrive))
+		iterator += 1
+	}
+   
+	return iterator
+}
+
+/**
+ * Move to node following instruction
+ */
+const step = (currentNode: string, instruction: string, map: Map) => {
+	const direction = getDirection(instruction)
+	return (map[currentNode] as any)[direction]
+}
+
+/**
+ * Translate instruction to direction
+ */
+const getDirection = (direrction: string) => {
+	if (direrction === 'L') return 'left'
+	return 'rigth'
+}
