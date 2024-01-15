ba35546e82c81c395e849a41f64187fdd95de6e0
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-01-15 03:39:11 +0100

    test: least common multiple
    failing test

---
+++ src/day-8/leastCommonMultiple.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it } from "node:test"
+import assert from "node:assert/strict"
+import { leastCommonMultiple } from "./leastCommonMultiple"
+
+void describe("leastCommonMultiple", () => {
+    const array = [1,2,3]
+    const expected = 12
+    void it(`should return ${expected} (Least Common Multiple) from ${array}`, () => {
+        assert.equal(leastCommonMultiple(array), expected)
+    })
+})
---
+++ src/day-8/leastCommonMultiple.ts
@@ -0,0 +1,6 @@
+/**
+ * the smallest number that two or more numbers can divide into evenly
+ */
+export const leastCommonMultiple = (array: number[]) => {
+    return 12
+}
