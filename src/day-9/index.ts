da85c7c45842a3b673f36ead91fb9088c2a64bf6
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-04-27 19:50:45 +0200

    WIP: predict next value from sequention
    failing test

---
+++ src/day-9/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Predict next number in secuention
+ * 
+ * 1. Make a new sequence from the difference at each step of input data. 
+ * 2. If that sequence is not all zeroes, repeat this process, using the sequence you just generated as the input sequence. 
+ * 3. Once all of the values in your latest sequence are zeroes, you can extrapolate what the next value of the original history should be
+ */
+export const predict = (input: number[]): number => {
+    return 0
+}
