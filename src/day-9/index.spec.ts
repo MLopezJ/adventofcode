b8c7a9ff005ddb84d255e41179a2e84547068115
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-04-27 20:05:53 +0200

    test: diff at each step
    Failing test.
    
    Make a new sequence from the difference at each step of input data.

--- src/day-9/index.spec.ts
+++ src/day-9/index.spec.ts
@@ -1,11 +1,21 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
-import { predict } from '.'
+import { differenceAtEachStep, predict } from '.'
 
 void describe('Predict', () => {
 	it(`should predict next value from sequention`, () => {
 		const input = [0, 3, 6, 9, 12, 15]
 		const expectedResult = 18
 		assert.equal(predict(input), expectedResult)
+	})
+})
+
+void describe(`difference at each step`, () => {
+	it(`should make a new sequence from the difference at each step of input data`, () => {
+		const input = [0, 3, 6, 9, 12, 15]
+		const expectedResult = [3, 3, 3, 3, 3]
+		const result = differenceAtEachStep(input)
+		assert.equal(result, expectedResult)
+		assert.equal(result.length, input.length - 1)
 	})
 })
--- src/day-9/index.ts
+++ src/day-9/index.ts
@@ -6,5 +6,12 @@
  * 3. Once all of the values in your latest sequence are zeroes, you can extrapolate what the next value of the original history should be
  */
 export const predict = (input: number[]): number => {
+    // 1. Make a new sequence from the difference at each step of input data. 
+
     return 0
+}
+
+// Make a new sequence from the difference at each step of input data. 
+export const differenceAtEachStep = (input: number[]): number[] => {
+    return [0]
 }
