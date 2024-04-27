fa363a0339866a9e75171ca3702b0432441c0ed5
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-04-27 21:40:07 +0200

    feat: predict next value from sequention

--- src/day-9/index.spec.ts
+++ src/day-9/index.spec.ts
@@ -1,12 +1,12 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
-import { differenceAtEachStep, predict } from '.'
+import { differenceAtEachStep, index } from '.'
 
 void describe('Predict', () => {
 	it(`should predict next value from sequention`, () => {
 		const input = [0, 3, 6, 9, 12, 15]
 		const expectedResult = 18
-		assert.equal(predict(input), expectedResult)
+		assert.equal(index(input), expectedResult)
 	})
 })
 
--- src/day-9/index.ts
+++ src/day-9/index.ts
@@ -1,17 +1,40 @@
 /**
+ * Predict next number in secuention 
+ */
+export const index = (input: number[]): number => {
+	const result = predict(input)
+	const prediction = result[result.length-1] // number predicted
+	return prediction as number
+}
+
+/**
  * Predict next number in secuention
  *
  * 1. Make a new sequence from the difference at each step of input data.
  * 2. If that sequence is not all zeroes, repeat this process, using the sequence you just generated as the input sequence.
  * 3. Once all of the values in your latest sequence are zeroes, you can extrapolate what the next value of the original history should be
  */
-export const predict = (input: number[]): number => {
-    // 1. Make a new sequence from the difference at each step of input data. 
+export const predict = (input: number[]): number[] => {
+	console.log(input)
+	if (areAllZeroes(input) === true){
+		// predict next element in sequention of 0s
+		//predict([...input, 0])
+		console.log('fin: ', [...input, 0])
+		return [...input, 0] as any // FIXME
+	} else {
+		// create new sequence
+		const newSequence = predict(differenceAtEachStep(input))
 
-    return 0
+		if (Array.isArray(newSequence) && newSequence.length === input.length){
+			console.log(newSequence, input)
+			const prediction = (newSequence[newSequence.length-1] as number) + ((input)[input.length-1] as number)
+			console.log(prediction)
+			return [...input, prediction]
+		}
+	}
 }
 
-// Make a new sequence from the difference at each step of input data. 
+// Make a new sequence from the difference at each step of input data.
 export const differenceAtEachStep = (input: number[]): number[] => {
 	const newSequence = input
 		.map((value, index, array) => {
